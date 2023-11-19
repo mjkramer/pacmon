d6b41d04b2df331ca10448e1fba44477eea23fc0
Matt Kramer <mjkramer@users.noreply.github.com>
2023-11-19 10:45:14 -0800

    working monitor

--- monitor_pacman.py
+++ monitor_pacman.py
@@ -2,6 +2,9 @@
 
 import zmq
 
+from format import Message
+
+
 def get_data_socket() -> zmq.Socket:
     ctx = zmq.Context()
     socket = ctx.socket(zmq.SUB)
@@ -16,13 +19,25 @@ def get_data_socket() -> zmq.Socket:
     socket.setsockopt(zmq.SUBSCRIBE, b'')
     return socket
 
+
 def get_data_poller() -> zmq.Poller:
     socket = get_data_socket()
     poller = zmq.Poller()
     poller.register(socket, zmq.POLLIN)
     return poller
 
-def dump_messages(poller: zmq.Poller):
+
+def dump_messages(pretty=False):
+    poller = get_data_poller()
     while True:
         for socket, _ in poller.poll():
-            print(socket.recv())
+            raw = socket.recv()
+            msg = Message.parse(raw)
+            if pretty:
+                print(msg)
+            else:
+                print(raw.hex(sep=' '))
+
+
+if __name__ == '__main__':
+    dump_messages()
