dbd03841d2d4968a132412d76ba2c3c87adae155
Matt Kramer <mjkramer@users.noreply.github.com>
2023-11-19 08:00:11 -0800

    cleanup

--- format.py
+++ format.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python3
 
-from construct import *
+from construct import (
+    Array, Byte, Bytes, Enum, Int16ub, Int32ub, Padding, Struct, Switch, this
+)
 
-data = Struct(
+Data = Struct(
     'io_channel' / Byte,
     'timestamp' / Int32ub,
     Padding(2),
     'packet' / Bytes(8)
 )
 
-trig = Struct(
+Trig = Struct(
     'type' / Byte,
     Padding(2),
     'timestamp' / Int32ub
 )
 
-sync = Struct(
+Sync = Struct(
     'type' / Byte,
     'clk_source' / Byte,
     'timestamp' / Int32ub
 )
 
-ping = Struct(
+Ping = Struct(
     Padding(15)
 )
 
-write = Struct(
+Write = Struct(
     Padding(3),
     'write1' / Int32ub,
     Padding(4),
     'write2' / Int32ub
 )
 
-read = Struct(
+Read = Struct(
     Padding(3),
     'read1' / Int32ub,
     Padding(4),
     'read2' / Int32ub
 )
 
-error = Struct(
+Error = Struct(
     'err' / Byte,
     Padding(14)                 # string?
 )
 
-wordtype = Enum(Byte,
+WordType = Enum(Byte,
     Data = ord('D'),
     Trig = ord('T'),
     Sync = ord('S'),
@@ -54,53 +56,53 @@ wordtype = Enum(Byte,
     Error = ord('E')
 )
 
-word = Struct(
-    'type' / wordtype,
+Word = Struct(
+    'type' / WordType,
     'content' / Switch(this.type,
         {
-            wordtype.Data: data,
-            wordtype.Trig: trig,
-            wordtype.Sync: sync,
-            wordtype.Ping: ping,
-            wordtype.Write: write,
-            wordtype.Read: read,
-            wordtype.Error: error,
+            WordType.Data: Data,
+            WordType.Trig: Trig,
+            WordType.Sync: Sync,
+            WordType.Ping: Ping,
+            WordType.Write: Write,
+            WordType.Read: Read,
+            WordType.Error: Error,
         }
     )
 )
 
-msgtype = Enum(Byte,
+MsgType = Enum(Byte,
     Data = ord('D'),
     Request = ord('?'),
     Reply = ord('!')
 )
 
-message = Struct(
-    'type' / msgtype,
+Message = Struct(
+    'type' / MsgType,
     'timestamp' / Int32ub,
     Padding(1),
     'num_words' / Int16ub,
-    'words' / Array(this.num_words, word)
+    'words' / Array(this.num_words, Word)
 )
 
 def test_parse():
     import io
     stream = b'D\x00\x00\x04\xd2\x00\x00\x00D\x00\x00\x04\xf2\x00\x00\x00'
     stream = io.BytesIO(stream)
-    m1 = message.parse_stream(stream)
-    m2 = message.parse_stream(stream)
+    m1 = Message.parse_stream(stream)
+    m2 = Message.parse_stream(stream)
     return m1, m2
 
 def test_build():
-    message.build({'type': msgtype.Data,
+    Message.build({'type': MsgType.Data,
                    'timestamp': 1234,
                    'num_words': 0,
                    'words': []})
-    w = {'type': wordtype.Data,
+    w = {'type': WordType.Data,
          'content': {'io_channel': 3,
                      'timestamp': 4321,
                      'packet': 666666}}
-    message.build({'type': msgtype.Data,
+    Message.build({'type': MsgType.Data,
                    'timestamp': 1234,
                    'num_words': 1,
                    'words': [w]})
